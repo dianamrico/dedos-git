14d843281e96cbe2aa23301b32344d2d1835cd3a
Du0073 <eds.colombia2@gmail.com>
2024-07-26 07:13:48 -0500

    dedo2 button

---
+++ dedo2.js
@@ -0,0 +1,7 @@
+const btnD2 = document.getElementById("dedo2")
+const text = document.getElementById("dedo2t")
+
+btnD2.addEventListener("click", ()=>{
+    //console.log("dedo 2")
+    text.innerText = "este lo cocinó"
+})
