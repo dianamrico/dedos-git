7bab710307d510d22ecca23a01c4cd739fcec4b5
Jithgarm <jonathansmg919@gmail.com>
2024-07-26 07:36:26 -0500

    Agregando dedo 3

---
+++ dedo3.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("dedo3")
+const text = document.getElementById("textodedo3")
+
+btn.addEventListener("click", ()=>{
+    text.innerText = "Este le echó la sal"
+})
